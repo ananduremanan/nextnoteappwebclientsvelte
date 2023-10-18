5c509717abf5cbcddf7fcff2977fe8094373bd25
ananduremanan <48993261+ananduremanan@users.noreply.github.com>
2023-10-18 15:27:11 +0530

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <h1>NextNote App</h1>
+</main>
+
+<style>
+  
+</style>
